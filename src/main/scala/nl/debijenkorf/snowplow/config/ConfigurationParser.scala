6e0cdeb37612682bf8b8c2cadc070a5a3c770d29
Martijn Zwennes <martijn.zwennes@debijenkorf.nl>
2018-09-25 10:11:31 +0200

    supply required params and fallbacks (env variables) to scopt

--- src/main/scala/nl/debijenkorf/snowplow/config/ConfigurationParser.scala
+++ src/main/scala/nl/debijenkorf/snowplow/config/ConfigurationParser.scala
@@ -5,23 +5,31 @@ class ConfigurationParser {
   private val parser = new scopt.OptionParser[Configuration]("gss") {
     head("google-storage-sink", "v0.1")
 
-    opt[String]('p', "project").action((x, c) =>
-      c.copy(projectId = x)).text("Google Project ID")
+    opt[String]('p', "project").action((x, c) => c
+      .copy(projectId = x)).text("Google Project ID")
+      .required().withFallback(() => sys.env("GOOGLE_PROJECT_ID"))
 
-    opt[String]('t', "topic").action((x, c) =>
-      c.copy(topicId = x)).text("Google Pub/Sub topic name")
+    opt[String]('t', "topic").action((x, c) => c
+      .copy(topicId = x)).text("Google Pub/Sub topic name")
+      .required().withFallback(() => sys.env("GOOGLE_TOPIC_ID"))
 
-    opt[String]('s', "sub").action((x, c) =>
-      c.copy(subscriptionId = x)).text("Google Pub/Sub subscription name")
+    opt[String]('s', "sub").action((x, c) => c
+      .copy(subscriptionId = x)).text("Google Pub/Sub subscription name")
+      .required().withFallback(() => sys.env("GOOGLE_SUBSCRIPTION_ID"))
 
-    opt[String]('b', "bucket").action((x, c) =>
-      c.copy(bucketName = x)).text("Google Storage bucket name")
+    opt[String]('b', "bucket").action((x, c) => c
+      .copy(bucketName = x)).text("Google Storage bucket name")
+      .required().withFallback(() => sys.env("GOOGLE_BUCKET_NAME"))
 
-    opt[Int]('m', "max").action((x, c) =>
-      c.copy(maxRecords = x)).text("Max records per generated file")
+    opt[Int]('m', "max").action((x, c) => c
+      .copy(maxRecords = x)).text("Max records per generated file")
+      .required().withFallback(() => sys.env("MAX_RECORDS_IN_FILE") toInt)
 
-    opt[String]('a', "auth").action((x, c) =>
-      c.copy(auth = x)).text("Location of the generated Google secret key")
+    opt[String]('a', "auth").action((x, c) => c
+      .copy(auth = x)).text("Location of the generated Google secret key")
+      .required().withFallback(() => sys.env("SECRET_AUTH_LOCATION"))
+
+    help("help").text("prints this usage text")
   }
 
   def parse(args: Array[String]): Option[Configuration] =
