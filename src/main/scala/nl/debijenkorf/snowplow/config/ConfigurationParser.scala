58605fd36cdb70f76e1696fa185098d449d87739
Martijn Zwennes <martijn.zwennes@debijenkorf.nl>
2018-10-03 10:03:09 +0200

    chore: removed env variables retrieval from conf parsing (hard to test)

--- src/main/scala/nl/debijenkorf/snowplow/config/ConfigurationParser.scala
+++ src/main/scala/nl/debijenkorf/snowplow/config/ConfigurationParser.scala
@@ -9,28 +9,26 @@ object ConfigurationParser {
 
     opt[String]('s', "sub").action((x, c) => c
       .copy(subscriptionId = x)).text("Google Pub/Sub subscription name")
-      .required().withFallback(() => sys.env("GOOGLE_SUBSCRIPTION_ID"))
+      .required()
 
     opt[String]('b', "bucket").action((x, c) => c
       .copy(bucketName = x)).text("Google Storage bucket name")
-      .required().withFallback(() => sys.env("GOOGLE_BUCKET_NAME"))
+      .required()
 
     opt[Int]('r', "rows").action((x, c) => c
-      .copy(maxRecords = x)).text("Max records per generated file")
-      .required().withFallback(() => sys.env("EMPTY_BUFFER_ROWS") toInt)
+      .copy(maxRecords = x)).text("Max records per generated file (default: 1000 rows)")
+      .required().withFallback(() => 1000)
 
     opt[Int]('m', "minutes").action((x, c) => c
-      .copy(maxMinutes = x)).text("Max minutes before sink empties")
-      .required().withFallback(() => sys.env("EMPTY_BUFFER_MINUTES") toInt)
+      .copy(maxMinutes = x)).text("Max minutes before sink empties (default: 60 minutes)")
+      .required().withFallback(() => 60)
 
     opt[String]('f', "secret").action((x, c) => c
-      .copy(secretLocation = x)).text("Location of Google Service account key")
+      .copy(secretLocation = x)).text("Location of Google Service account key (default: /etc/secret.json)")
       .validate(x =>
         if (Files.exists(Paths.get(x))) success
         else failure(s"given file location does not exist: $x"))
-      .required().withFallback(() => sys.env("SECRET_KEY_LOCATION"))
-
-    help("help").text("Prints this usage text")
+      .required().withFallback(() => "/etc/secret.json")
   }
 
   def parse(args: Array[String]): Option[Configuration] =
